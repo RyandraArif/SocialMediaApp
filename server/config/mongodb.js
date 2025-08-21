9606e5741f73020ceb4361f9fee771dd783da459
RyandraArif <ryandraarif@gmail.com>
2025-08-21 21:41:17 +0700

    Update MongoDB connection string to use environment variable for improved security

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,6 +1,5 @@
 const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://ryandraarif:7hswQHeIpTVV0obY@cluster0.z65tliz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri);
 const database = client.db("socialMedia");
